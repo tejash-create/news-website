d28713abd129041bdbf0bb5a4de7c73c9bf393c4
Tejash <tejash.singh@telusinternational.com>
2024-07-15 22:27:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          This is the first app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
